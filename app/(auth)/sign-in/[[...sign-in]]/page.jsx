8a6f7c27159d62a119e92d421dd748f97ceed596
vyshnavkv <vyshnavkv89@gmail.com>
2025-04-09 13:37:14 +0530

    Added clerk authentication configurations

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -0,0 +1,13 @@
+import { SignIn } from '@clerk/nextjs'
+import React from 'react'
+
+function SignInPage() {
+  return (
+    <div>
+        <SignIn />
+      
+    </div>
+  )
+}
+
+export default SignInPage
